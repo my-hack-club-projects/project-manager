efe308d905a5ec83fc67711b2eaaa233e93b46fa
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-10 18:55:31 +0200

    Fixed TS import errors

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// @ts-ignore
 import LandingPageLayout from '../layouts/LandingPageLayout.vue'
-// @ts-ignore
 import ProjectManagerLayout from '../layouts/ProjectManagerLayout.vue'
-// @ts-ignore
 import HomeView from '../views/HomeView.vue'
-// @ts-ignore
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,12 +13,9 @@ const router = createRouter({
       component: LandingPageLayout,
       children: [
         { path: '', component: HomeView },
-        // @ts-ignore
         { path: 'about', component: () => import('../views/AboutView.vue') },
-        // @ts-ignore
         { path: 'contact', component: () => import('../views/ContactView.vue') },
 
-        // @ts-ignore
         { path: 'logintest', component: () => import('../views/LoginTest.vue')},
         // Other routes that should be displayed in the LandingPageLayout
       ]
@@ -34,11 +27,8 @@ const router = createRouter({
       name: 'projects',
       component: ProjectManagerLayout,
       children: [
-        // @ts-ignore
         { path: '', component: () => import('../views/ProjectListView.vue') },
-        // @ts-ignore
         { path: 'analytics', component: () => import('../views/ProjectAnalytics.vue')},
-        // @ts-ignore
         { path: 'view/:id(\\d+)', component: () => import('../views/ProjectView.vue') },
         
         // Other routes that should be displayed in the ProjectManagerLayout
