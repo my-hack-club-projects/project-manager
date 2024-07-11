0238ed8b52813292e476b031356e17bbd685c644
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-11 11:27:33 +0200

    Fetch data from server and format it

--- frontend/src/views/ProjectView.vue
+++ frontend/src/views/ProjectView.vue
@@ -5,8 +5,8 @@
     <div class="container mx-auto mt-6">
       <h2 class="text-2xl font-bold mb-4 mt-2">Tasks</h2>
       <div class="flex flex-col">
-        <TaskContainer />
-        <TaskContainer />
+        <TaskContainer v-for="(container, index) in taskContainers" :key="index" :title="container.title"
+          :is_completed="container.is_completed" :tasks="container.tasks" />
       </div>
     </div>
   </div>
@@ -15,11 +15,35 @@
 <script>
 import TaskContainer from '../components/task/TaskContainer.vue'
 import SessionInfo from '../components/project/SessionInfo.vue'
+import axios from 'axios'
 
 export default {
   components: {
     TaskContainer,
     SessionInfo
+  },
+  data() {
+    return {
+      taskContainers: []
+    }
+  },
+  async created() {
+    const projectId = this.$route.params.projectId
+    try {
+      // Get all task containers
+      const { data: containers } = await axios.get(`/api/projects/${projectId}/taskcontainers`)
+      // Get all tasks within them
+      const containersWithTasks = await Promise.all(containers.map(async container => {
+        const { data: tasks } = await axios.get(`/api/taskcontainers/${container.id}/tasks`)
+        return { ...container, tasks }
+      }))
+      // Log the data to the console (formatted as JSON)
+      console.log(JSON.stringify(containersWithTasks, null, 2))
+      // Set the taskContainers data property to the processed data
+      this.taskContainers = containersWithTasks
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
   }
 }
 </script>
