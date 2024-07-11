57dbcbbebbfd3185411aa5bb5fff29443f5e7377
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-11 16:59:19 +0200

    Better deploy script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,24 @@
+#!/bin/bash
+
+# Find the root of the project
+gitroot=$(git rev-parse --show-toplevel)
+
+cd "$gitroot/frontend"
+# Build frontend
+npm run test
+npm run build
+
+cd "$gitroot/backend"
+
+# Collect static files
 python3 manage.py collectstatic --noinput
+
+# Migrate
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+# Run tests and server
+python3 manage.py test
 python3 manage.py runserver
+
+cd $gitroot
