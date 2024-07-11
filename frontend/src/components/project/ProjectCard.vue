01077ba99185ecc460c2bc9d3444f868d346fb2c
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-11 17:58:05 +0200

    Hydrate projects list page

--- frontend/src/components/project/ProjectCard.vue
+++ frontend/src/components/project/ProjectCard.vue
@@ -2,6 +2,14 @@
 
 <template>
     <button class="bg-gradient-to-br from-cyan-300 to-blue-600 rounded-lg shadow-lg p-4 aspect-video text-left">
-        <h3 class="text-lg text-white font-bold h-full">Project title</h3>
+        <h3 class="text-lg text-white font-bold h-full">{{ project.name }}</h3>
     </button>
 </template>
+
+<script>
+export default {
+    props: {
+        project: Object
+    }
+}
+</script>
--- frontend/src/views/ProjectListView.vue
+++ frontend/src/views/ProjectListView.vue
@@ -7,7 +7,8 @@ import ProjectCard from '@/components/project/ProjectCard.vue'
     <div class="container mx-auto p-4">
       <h2 class="text-2xl font-bold mb-4 mt-2">Your projects</h2>
       <div class="flex flex-col">
-        <div class="py-4">
+        <!-- Inside this container are the categories such as the one below -->
+        <!-- <div class="py-4">
           <h3 class="text-sm font-bold mb-2">Category 1</h3>
 
           <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -15,21 +16,44 @@ import ProjectCard from '@/components/project/ProjectCard.vue'
             <ProjectCard />
             <ProjectCard />
           </div>
-        </div>
+        </div> -->
 
-        <div class="py-4">
-          <h3 class="text-sm font-bold mb-2">Category 2</h3>
+        <!-- Iterate through this.categories and hydrate the html -->
+
+        <div v-for="(category, index) in categories" :key="index" class="py-4">
+          <h3 class="text-sm font-bold mb-2">{{ category.name }}</h3>
 
           <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+            <ProjectCard v-for="(project, index) in category.projects" :key="index" :project="project" />
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+    ProjectCard
+  },
+  data() {
+    return {
+      categories: []
+    }
+  },
+  async created() {
+    const categories = await this.$http.get('/api/categories/')
+
+    for (const category of categories.data) {
+      const projects = await this.$http.get(`/api/projects/?category=${category.id}`)
+      category.projects = projects.data
+
+      this.categories.push(category)
+    }
+  }
+}
+</script>
 
 <style>
 .container {
