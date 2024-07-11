c026eaeadd98b03528623cb86465c66b0cfd39b3
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-11 12:31:02 +0200

    Add edit button

--- frontend/src/components/task/TaskContainer.vue
+++ frontend/src/components/task/TaskContainer.vue
@@ -2,7 +2,17 @@
     <div class="container my-5">
         <div class="w-full">
             <div class="bg-slate-200 rounded-lg shadow-md p-6 w-full">
-                <h1 class="text-lg font-bold">{{ title }}</h1>
+                <div class="flex items-center">
+                    <h1 class="text-lg font-bold">{{ title }}</h1>
+                    <button class="ml-2">
+                        <svg class="feather feather-edit" fill="none" height="24" stroke="currentColor"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                            width="24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                        </svg>
+                    </button>
+                </div>
                 <TaskList :tasks="tasks" @delete-task="deleteTask" @edit-task="editTask"
                     @toggle-complete="toggleComplete" />
                 <AddTaskForm @add-task="addTask" />
