65ec962042cec058637ad97fe7c55bca2cdf75f4
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-11 18:02:59 +0200

    Removed comments

--- frontend/src/views/ProjectListView.vue
+++ frontend/src/views/ProjectListView.vue
@@ -7,19 +7,6 @@ import ProjectCard from '@/components/project/ProjectCard.vue'
     <div class="container mx-auto p-4">
       <h2 class="text-2xl font-bold mb-4 mt-2">Your projects</h2>
       <div class="flex flex-col">
-        <!-- Inside this container are the categories such as the one below -->
-        <!-- <div class="py-4">
-          <h3 class="text-sm font-bold mb-2">Category 1</h3>
-
-          <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-          </div>
-        </div> -->
-
-        <!-- Iterate through this.categories and hydrate the html -->
-
         <div v-for="(category, index) in categories" :key="index" class="py-4">
           <h3 class="text-sm font-bold mb-2">{{ category.name }}</h3>
 
