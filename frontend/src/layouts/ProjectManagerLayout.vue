1579f3ba444f9ada29edb27bc3af8b2ea4c23e71
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-11 09:38:02 +0200

    Improved project list design

--- frontend/src/layouts/ProjectManagerLayout.vue
+++ frontend/src/layouts/ProjectManagerLayout.vue
@@ -1,22 +1,24 @@
 <template>
-  <header class="bg-gray-800 text-white py-4 px-6">
-    <div class="flex justify-between items-center">
-      <div class="flex items-center">
-      <h1 class="text-2xl font-bold mr-4">Project Manager</h1>
-      <nav>
-        <ul class="flex space-x-4">
-        <li><router-link to="/projects">Projects</router-link></li>
-        <li><router-link to="/projects/analytics">Analytics</router-link></li>
-        </ul>
-      </nav>
+  <div class="flex h-screen flex-col">
+    <header class="bg-gray-800 text-white py-4 px-6">
+      <div class="flex justify-between items-center">
+        <div class="flex items-center">
+          <h1 class="text-2xl font-bold mr-4">Project Manager</h1>
+          <nav>
+            <ul class="flex space-x-4">
+              <li><router-link to="/projects">Projects</router-link></li>
+              <li><router-link to="/projects/analytics">Analytics</router-link></li>
+            </ul>
+          </nav>
+        </div>
+        <button class="rounded-full bg-white w-8 h-8"></button>
       </div>
-      <button class="rounded-full bg-white w-8 h-8"></button>
-    </div>
-  </header>
+    </header>
 
-  <main>
-    <router-view></router-view>
-  </main>
+    <main class="flex-1">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <script>
--- frontend/src/views/ProjectListView.vue
+++ frontend/src/views/ProjectListView.vue
@@ -3,16 +3,28 @@ import ProjectCard from '@/components/project/ProjectCard.vue'
 </script>
 
 <template>
-  <div class="container mx-auto p-4">
-    <h2 class="text-lg font-bold">Your projects</h2>
-    <div class="flex flex-col border-t-2 border-black w-full">
-      <div class="py-4">
-        <h3 class="text-sm font-bold mb-2">Category 1</h3>
-        
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+  <div class="bg-gray-100 h-full">
+    <div class="container mx-auto p-4">
+      <h2 class="text-2xl font-bold mb-4 mt-2">Your projects</h2>
+      <div class="flex flex-col">
+        <div class="py-4">
+          <h3 class="text-sm font-bold mb-2">Category 1</h3>
+
+          <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </div>
+        </div>
+
+        <div class="py-4">
+          <h3 class="text-sm font-bold mb-2">Category 2</h3>
+
+          <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </div>
         </div>
       </div>
     </div>
