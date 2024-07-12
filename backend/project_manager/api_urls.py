9bb834cc8aad8f47c5d1f8776ee11cad6130d0fa
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-12 18:38:56 +0200

    remove commented code

--- backend/project_manager/api_urls.py
+++ backend/project_manager/api_urls.py
@@ -1,21 +1,3 @@
-# # project_manager/urls.py
-# from django.urls import path
-# from .views_old import *
-
-# urlpatterns = [
-#     path('categories/', UserCategoriesAPIView.as_view(), name='user-categories'),
-#     path('categories/<int:pk>/projects/', CategoryProjectsAPIView.as_view(), name='category-projects'),
-#     path('projects/<int:pk>/taskcontainers/', ProjectTaskContainersAPIView.as_view(), name='project-task-containers'),
-#     path('projects/<int:pk>/sessions/', ProjectSessionsAPIView.as_view(), name='project-sessions'),
-#     path('sessions/<int:pk>/notes/', SessionNotesAPIView.as_view(), name='session-notes'),
-#     path('taskcontainers/<int:pk>/tasks/', TaskContainerTasksAPIView.as_view(), name='task-container-tasks'),
-#     # old endpoints above, bad structure
-#     # writing new ones below
-#     # the structure should be: GET resource/identifier/ that returns a json of the resource, serialized
-#     # POST resource/ that creates a new resource
-#     # PUT, DELETE resource/identifier/ that updates or deletes the resource
-# ]
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProjectViewSet, TaskContainerViewSet, SessionViewSet, TaskViewSet, NoteViewSet
