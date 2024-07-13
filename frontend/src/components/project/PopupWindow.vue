138e19a6431c9eff3c053c0cd32ac430d182aa4f
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-13 13:21:21 +0200

    Placeholder text for popup window

--- frontend/src/components/project/PopupWindow.vue
+++ frontend/src/components/project/PopupWindow.vue
@@ -2,11 +2,11 @@
     <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
         <div class="bg-white w-full sm:w-5/6 md:w-3/4 p-6 rounded-lg shadow-lg">
             <div class="flex justify-between mb-2">
-                <input class="text-2xl font-bold" v-if="titleEditable" v-model="title"
+                <input class="text-2xl font-bold" placeholder="No title" v-if="titleEditable" v-model="title"
                     @input="$emit('change', title, description)" />
                 <button @click="$emit('close')" class="text-2xl">&times;</button>
             </div>
-            <textarea class="w-full" v-if="descriptionEditable" v-model="description"
+            <textarea class="w-full" placeholder="No description" v-if="descriptionEditable" v-model="description"
                 @input="$emit('change', title, description)" />
             <slot></slot>
         </div>
