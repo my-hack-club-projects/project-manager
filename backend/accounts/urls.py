c192989222c0e61127657aba2ac541b03c20acc5
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-15 14:34:25 +0200

    Email verification working

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
-from .views import CustomRegisterView, CustomConfirmEmailView, SetPasswordView
+from django.urls import path, re_path, include
+from allauth.account.views import confirm_email as allauthemailconfirmation
+# from .views import CustomRegisterView, CustomConfirmEmailView, SetPasswordView
+
+import regex
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
+    path('rest-auth/registration/account-confirm-email/(?P<key>{0})/', allauthemailconfirmation, name="account_confirm_email"),
 ]
