464cc0636dd53964c17a5b17003af17f91913386
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-15 16:52:40 +0200

    Added login functionality

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- todo disable scrolling -->
     <div class="flex flex-col items-center h-screen bg-gray-50">
-        <LoginForm type="login"></LoginForm>
+        <LoginForm @login="login" type="login"></LoginForm>
     </div>
 </template>
 
@@ -11,6 +11,28 @@ import LoginForm from '@/components/global/LoginForm.vue';
 export default {
     components: {
         LoginForm
+    },
+
+    methods: {
+        login(data) {
+            const email = data.email;
+            const password = data.password1;
+
+            this.$http.post('/accounts/login/', {
+                email: email,
+                password: password
+            }).then(response => {
+                if (!response.data.success) {
+                    throw new Error(response.data.message);
+                }
+
+                this.$router.push("/projects/");
+            }).catch(error => {
+                console.log(error.response.data);
+
+                this.$alert(error.response.data)
+            });
+        }
     }
 };
 </script>
