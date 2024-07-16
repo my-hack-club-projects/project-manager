a9d14aaf98641127e64cd99b1c9b29386d7cef88
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-16 15:42:13 +0200

    fix

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,7 +3,7 @@ from allauth.account.views import confirm_email as allauthemailconfirmation
 from .views import CustomConfirmEmailView, GitHubLogin
 
 urlpatterns = [
-    path('', include('allauth.urls')),
+    # path('', include('allauth.urls')),
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     re_path(r'^email_confirmation/(?P<key>.+)/$', CustomConfirmEmailView.as_view(), name="account_confirm_email"),
