a2d2bc0692666603290ea8a667dd923d3b9f4956
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-16 15:12:30 +0200

    Auto-create categories for new users

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from project_manager.models import Category
 from django.db import models
 
 class CustomUser(AbstractUser):
-    default_category = models.IntegerField(default=0)
-    archive_category = models.IntegerField(default=0)
+    default_category = models.OneToOneField(Category, on_delete=models.CASCADE, null=True, blank=True)
+    archive_category = models.OneToOneField(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def save(self):
+        self.super.save()
+
+        if self.default_category is None:
+            self.default_category = Category.objects.create(user=self, name="Active Projects")
+            self.default_category.save()
+
+        if self.archive_category is None:
+            self.archive_category = Category.objects.create(user=self, name="Archived Projects", locked=True)
+            self.archive_category.save()
