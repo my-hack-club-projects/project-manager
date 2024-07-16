3a261bf4f1136762e33424f05fd8f5d8d79fbfb8
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-16 11:01:57 +0200

    Fixed path warning

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from allauth.account.views import confirm_email as allauthemailconfirmation
 from .views import CustomConfirmEmailView, GitHubLogin
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
-    path('email_confirmation/(?P<key>{0})/', CustomConfirmEmailView.as_view(), name="account_confirm_email"),
+    re_path(r'^email_confirmation/(?P<key>.+)/$', CustomConfirmEmailView.as_view(), name="account_confirm_email"),
 
     path('github/login/callback/', GitHubLogin.as_view(), name='github_login'),
 ]
