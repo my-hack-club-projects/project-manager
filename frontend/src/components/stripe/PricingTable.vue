36545d47694a102916d311e3ce429d2d5325beaa
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-17 14:53:56 +0200

    Created stripe pricing table component

---
+++ frontend/src/components/stripe/PricingTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div v-if="stripePublicKey && stripePricingTableId">
+            <stripe-pricing-table :pricing-table-id="stripePricingTableId" :publishable-key="stripePublicKey">
+            </stripe-pricing-table>
+        </div>
+        <div v-else>
+            Loading...
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            stripePricingTableId: null,
+            stripePublicKey: null,
+            isLoaded: false,
+        };
+    },
+    mounted() {
+        this.fetchKeys();
+    },
+    methods: {
+        fetchKeys() {
+            this.$http.get('/api/products/keys/')
+                .then(response => {
+                    this.stripePricingTableId = response.data.data.pricing_table_id;
+                    this.stripePublicKey = response.data.data.public_key;
+                    console.log('Fetched keys:', this.stripePricingTableId, this.stripePublicKey);
+                    this.loadStripe();
+                })
+                .catch(error => {
+                    console.error('Error fetching keys:', error);
+                });
+        },
+        loadStripe() {
+            if (!window.Stripe) {
+                const script = document.createElement('script');
+                script.src = "https://js.stripe.com/v3/pricing-table.js";
+                script.async = true;
+                script.onload = this.initializeStripe;
+                document.head.appendChild(script);
+            } else {
+                this.initializeStripe();
+            }
+        },
+        initializeStripe() {
+            this.isLoaded = true;
+        }
+    }
+}
+</script>
