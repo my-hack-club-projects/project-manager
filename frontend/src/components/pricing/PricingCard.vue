d18435c7706f1ba001f0868e1809625809dab698
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-17 17:48:25 +0200

    Converted the pricing table into components

---
+++ frontend/src/components/pricing/PricingCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div
+        class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{{ title }}</h5>
+        <div class="flex items-baseline text-gray-900 dark:text-white">
+            <span class="text-3xl font-semibold">$</span>
+            <span class="text-5xl font-extrabold tracking-tight">{{ price }}</span>
+            <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+        </div>
+        <ul role="list" class="space-y-5 my-7">
+            <PricingFeature v-for="feature in features" :key="feature" :feature="feature" />
+        </ul>
+
+        <TextButton color="blue" class="w-full">Choose plan</TextButton>
+    </div>
+</template>
+
+<script>
+import TextButton from '@/components/global/TextButton.vue';
+import PricingFeature from './PricingFeature.vue';
+
+export default {
+    components: {
+        TextButton,
+        PricingFeature,
+    },
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        features: {
+            type: Array,
+            required: true,
+        },
+    }
+}
+</script>
---
+++ frontend/src/components/pricing/PricingTable.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col lg:flex-row items-start justify-center gap-4 w-full">
+        <PricingCard title="Standard" price="0" :features="['Feature 1', 'Feature 2', 'Feature 3']" />
+        <PricingCard title="Premium" price="9" :features="['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4']" />
+    </div>
+</template>
+
+<script>
+import PricingCard from '@/components/pricing/PricingCard.vue';
+
+export default {
+    components: {
+        PricingCard,
+    },
+}
+</script>
