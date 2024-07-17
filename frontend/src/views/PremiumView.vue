472892f043497731a8ab404a7d01dd7c16fb8038
robinafro <oble.ro.2023@skola.ssps.cz>
2024-07-17 16:39:34 +0200

    Added title

--- frontend/src/views/PremiumView.vue
+++ frontend/src/views/PremiumView.vue
@@ -1,5 +1,10 @@
 <template>
-  <PricingTable></PricingTable>
+  <div class="bg-gradient-to-br from-gray-800 to-black w-full h-full flex flex-col items-center justify-center">
+
+    <div class="flex items-center justify-center text-6xl font-semibold text-white py-12 w-full">
+      <span class="text-blue-500 font-extrabold glow">Supercharge</span>&nbsp;your productivity.
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,3 +16,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.glow {
+  text-shadow: 0rem 0rem 2rem rgb(2, 2, 255);
+}
+</style>
